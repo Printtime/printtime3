bdf4568f7aa9411c1d68b211a6f068c0fc48cf9c
deminua <dataranenko@gmail.com.ua>
2017-12-28 08:49:38 +0200

    api price

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,44 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/price', function (Request $request) {
+
+	header("Access-Control-Allow-Origin: http://php.loc");
+
+    $validation = Validator::make($request->all(),[ 
+        'type' => 'required|string',
+        'id' => 'required|integer',
+    ]);
+
+    if($validation->fails()){
+      return response()->json([
+		    'error' => ['errors'=>$validation->errors()],
+		    'code' => 404,
+		    'message' => "Not Found",
+		], 404);
+    }
+
+		if($request->type == 'products' && $request->id == '9') {
+			return DB::table('products')
+			->select('id', 'title')
+			->where('catalog_id', 9)->get();
+		}
+
+		if($request->type == 'types') {
+			return \App\Type::where('product_id', $request->id)->select('id', 'title')->get();
+		}
+
+		if($request->type == 'vars') {
+			return \App\Type::findOrfail($request->id)->variables()->orderBy('order', 'asc')->get();
+		}
+
+      return response()->json([
+		    'error' => ['errors'=>['price not found']],
+		    'code' => 404,
+		    'message' => "Not Found",
+		], 404);
+        // $typevars = \App\TypeVar::with('type', 'variable')->get();
+        // return $typevars;
+});
+
 // Route::middleware('can:admin')->get('/menu', 'MenuController@jsonMenu')->name('api.jsonMenu');
