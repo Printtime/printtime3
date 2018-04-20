10685a06beb8b9c24e89b06dfe2b0bacc00443e7
Printtime <office@printtime.com.ua>
2018-04-20 15:56:52 +0300

    sitemap

--- resources/views/page/sitemap.blade.php
+++ resources/views/page/sitemap.blade.php
@@ -1,10 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+header('Content-type: text/xml');
+print '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach($pages as $item)
      <url>
         <loc>https://printtime.com.ua/{{ $item->slug }}</loc>
       	<changefreq>{{ $item->changefreq }}</changefreq>
       	<priority>{{ $item->priority }}</priority>
+        <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
      </url>
     @endforeach
 </urlset>
